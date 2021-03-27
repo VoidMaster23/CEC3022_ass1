152cf8ee53705ed14cb81f2ef469434841565378
Edson Shivuri <edsonshivuri@gmail.com>
2021-03-27 17:09:59 +0200

    Queue populated

--- functions.cpp
+++ functions.cpp
@@ -33,12 +33,76 @@ namespace SHVNKA005 {
 }
 
 void processLine(string line, str_queue &inQueue, tag_vector &outVect) {
-std::cout << line << std::endl;
+string text = "";
+string tagText ="";
+
+std::regex tag ("<[^\\s*].+?>");
+//loop through each character 
+
+if(line[0] != '<' && line[0] != '\t') {
+        inQueue.push(line);
+    } else {
+
+    for(string::size_type i = 0; i < line.size(); ++i) {
+
+    
+
+    char myChar  = line[i];
+    if (myChar != '\t' && myChar != '\n'){
+        if (myChar == '<') {
+            tagText = "";
+
+        }
+        text += myChar;
+        tagText += myChar;
+
+        
+        if(std::regex_match (tagText, tag) && myChar == '>') {
+            
+          
+
+            if (tagText != text) {
+                //string contains the other string
+                auto startInd = text.find(tagText);
+                if (startInd != string::npos) {
+                   //  std::cout << "I AM HERE" << std::endl;
+                     text.replace(startInd, tagText.size(), " ");
+                }
+            }
+
+            // at this point place the tag into the queue
+            
+
+            if (!std::regex_match(text, tag)) {
+            inQueue.push(text);
+            
+            std::cout << text << std::endl;
+            std::cout << std::endl;
+            }
+
+            std::cout << tagText << std::endl;
+            inQueue.push(tagText);
+
+
+            text = "";
+            tagText = "";
+        }
+
+        
+    }
+    }
+}
+
+
 }
 
 
 void prepareOutput(str_queue &inputQueue, tag_vector &outputvector ) {
+    for (str_queue dump = inputQueue; !dump.empty(); dump.pop())
     
+        std::cout << dump.front() << std::endl;
+
+    std::cout << "(" << inputQueue.size() << " elements)\n";
 } 
 
 }   
--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-//#include "tagstruct"
+#include "tagstruct"
 #include "functions"
 #define str std::string
 #define str_queue std::queue<str>
-#define str_vect std::vector<str>
+#define tag_vector std::vector<SHVNKA005::tagStruct>
 
 int main(int argc, char* argv[]) {
     SHVNKA005::tagStruct test;
     str fileName;
     str_queue inQueue;
-    str_vect outVect;
+    tag_vector outVect;
 
     if(argc == 1)
         fileName = "simple.txt";
@@ -18,17 +18,18 @@ int main(int argc, char* argv[]) {
     
 
     SHVNKA005::readFile(fileName, inQueue, outVect);
+    SHVNKA005::prepareOutput(inQueue, outVect);
 
-    std::cout << outVect.size() << std::endl;
-    for (int i=0; i<outVect.size(); i++){
-       std::cout << outVect[i] << std::endl;
-    }
+    // std::cout << outVect.size() << std::endl;
+    // for (int i=0; i<outVect.size(); i++){
+    //    std::cout << outVect[i] << std::endl;
+    // }
 
     
-    for (str_queue dump = inQueue; !dump.empty(); dump.pop())
-        std::cout << dump.front() << std::endl;
+    // for (str_queue dump = inQueue; !dump.empty(); dump.pop())
+    //     std::cout << dump.front() << std::endl;
 
-    std::cout << "(" << inQueue.size() << " elements)\n";
+    // std::cout << "(" << inQueue.size() << " elements)\n";
 
 
     return 0;
